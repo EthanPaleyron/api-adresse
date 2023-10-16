23b970668d5c89953dd40698268037693bc9397c
Ethan <epaleyron@edenschool.fr>
2023-10-16 17:12:58 +0200

    fin de api

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const urlLocation = (location, codePostal) => {
+  let url = `https://api-adresse.data.gouv.fr/search/?q=${location}&postcode=${codePostal}`;
+
+  fetch(url)
+    .then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+        document.querySelector("#address").textContent = data.query;
+        document.querySelector("#postalCode").textContent =
+          data.filters.postcode;
+        document.querySelector("#city").textContent =
+          data.features[0].properties.city;
+        document.querySelector("#street").textContent =
+          data.features[0].properties.street;
+        document.querySelector("#region").textContent =
+          data.features[0].properties.context;
+        let lat = data.features[0].geometry.coordinates[1];
+        let lon = data.features[0].geometry.coordinates[0];
+        initMap([lat, lon]);
+      })
+    )
+    .catch((error) => console.log("Error : " + error));
+};
+
+const searchAddress = document.querySelector("#searchAddress");
+const searchPostalCode = document.querySelector("#searchPostalCode");
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  urlLocation(searchAddress.value, searchPostalCode.value);
+  searchAddress.value = "";
+  searchPostalCode.value = "";
+});
+
+let myCard;
+let marker;
+function initMap(coor) {
+  if (myCard) {
+    myCard.setView(coor, 11);
+    marker = L.marker(coor).addTo(myCard);
+  } else {
+    myCard = L.map("map").setView(coor, 11);
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
+      attribution:
+        'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+      minZoom: 1,
+      maxZoom: 20,
+    }).addTo(myCard);
+    marker = L.marker(coor).addTo(myCard);
+  }
+}
